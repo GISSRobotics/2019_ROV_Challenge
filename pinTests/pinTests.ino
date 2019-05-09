e0d4ac714718860054ea9fd4f1b27923583159b9
Bruce <br.apps@icloud.com>
2019-05-08 19:51:46 -0700

    Motor Unit Tests

---
+++ pinTests/pinTests.ino
@@ -0,0 +1,42 @@
+int BUZZER5 = 5;
+int BUZZER6 = 6;
+int BUZZER7 = 7;
+int BUZZER8 = 8;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  beep(BUZZER5, 32);
+  beep(BUZZER5, 32);
+  beep(BUZZER5, 32);
+
+  beep(BUZZER6, 64);
+  beep(BUZZER6, 64);
+  beep(BUZZER6, 64);
+  
+  beep(BUZZER7, 128);
+  beep(BUZZER7, 128);
+  beep(BUZZER7, 128);
+  
+  beep(BUZZER8, 256);
+  beep(BUZZER8, 256);
+  beep(BUZZER8, 256);
+
+}
+
+void beep(int pin, int frequency) {
+  Serial.println(pin);
+  digitalWrite(pin, HIGH); // sets the digital pin 13 on
+  delay(frequency);            // waits for a second
+  digitalWrite(pin, LOW);  // sets the digital pin 13 off
+  delay(frequency);  
+}
+
