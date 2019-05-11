13131fd117e0b76f8bb33b6c1f2319d7a1be48d0
Bruce <t15thbruce@outlook.de>
2019-05-11 13:24:38 -0700

    documentation

--- pinTests/pinTests.ino
+++ pinTests/pinTests.ino
@@ -1,5 +1,30 @@
 // SELECT 328P (old bootloader) WHEN USING USB C CONVERTER!
 
+/*
+ * To test RX-TX:
+ * Connect USB to FTTI to Tether in reverse. RX to TX and TX to RX. GND to GND
+ * If Serial Monitor outputs something, the Tether works.
+ * 
+ * To test Motors:
+ * Connect Buzzer to Motor Pins.
+ * D5 will beep every 12ms
+ * D6 will beep every 25ms
+ * D7 will beep every 50ms
+ * D8 will beep every 75ms
+ * 
+ * To test I2C:
+ * Connect Photo Resistor to I2C
+ * -> Signal to pin to be tested (either top A4 or bottom A5)
+ * -> GND to GND (third from top)
+ * -> 5V to 3V3 (second from top)
+ * If the output in the Serial Monitor changes correctly, the I2C is working.
+ * 
+ * To test Battery:
+ * Connect Battery to board
+ * -> Battery and Serial can be safely connected at the same time
+ * A7 will output battery voltage
+ */
+
 int BUZZER5 = 5;
 int BUZZER6 = 6;
 int BUZZER7 = 7;
