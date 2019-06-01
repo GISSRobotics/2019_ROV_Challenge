7ade0189826a880ea230bcdeb05604c7b684315c
Bruce <t15thbruce@outlook.de>
2019-06-01 10:57:25 -0700

    use lib to take pressure

--- I2Creadings/I2Creadings.ino
+++ I2Creadings/I2Creadings.ino
@@ -1,31 +1,45 @@
+#include <SparkFun_MS5803_I2C.h>
 #include <Wire.h>
+
+MS5803 sensor(ADDRESS_HIGH);
+
+double pressure_abs;
 
 void setup() {
   // put your setup code here, to run once:
   Wire.begin();
   Serial.begin(9600);
+  sensor.reset();
+  sensor.begin();
   reset();
-}
+} 
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  /*
   Wire.beginTransmission(0x76);
   Wire.write(0x48);
   Wire.endTransmission();
+  delay(500);
   Wire.beginTransmission(0x76);
   Wire.write(0x00);
   Wire.endTransmission();
+  delay(500);
   
-  int response;
+  uint32_t response;
   
   byte b = Wire.read();
-  response = b << 16;
-  byte b = Wire.read();
-  response |= b << 8;
-  byte b = Wire.read();
-  response |= b;
-  Serial.print(b);
-  
+  response = (uint32_t)b << 16;
+  b = Wire.read();
+  response |= (uint32_t)b << 8;
+  b = Wire.read();
+  response |= (uint32_t)b;
+  Serial.println(response);
+  */
+
+  pressure_abs = sensor.getPressure(ADC_4096);
+  Serial.println(pressure_abs);
   
   delay(100);
 }
@@ -35,4 +49,3 @@ void reset() {
   Wire.write(0x1E);
   Wire.endTransmission();
 }
-
